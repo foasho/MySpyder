ec94acbae3ab519be9a9bd3583dbea37bca6cc76
foasho <foa.sho0111@outlook.jp>
2022-02-09 08:07:21 +0900

    fix: LCDにIP表示処理追加

---
+++ startup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import time
+import netifaces
+from lcd_controls import show_lcd
+
+def startup():
+    ip_addres = None
+    interface_data = netifaces.ifaddresses('eth0')
+    for key, val in interface_data.items():
+        for v in val:
+            addr = v["addr"]
+            if "." in addr and "192" in addr and ip_addres == None:
+                ip_addres = addr
+    interface_data2 = netifaces.ifaddresses('wlan0')
+    for key, val in interface_data2.items():
+        for v in val:
+            addr = v["addr"]
+            if "." in addr and "192" in addr and ip_addres == None:
+                ip_addres = addr
+    print(f"取得したIPアドレス＝{ip_addres}")
+    show_lcd("[IP ADDRESS]", ip_addres)
+    print("Waiting1")
+    time.sleep(3)
+    print("Waiting2")
+    time.sleep(3)
+    print("Completed.")
+
+if __name__ == "__main__":
+    startup()
