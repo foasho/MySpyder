ec04ccf0ff8ca2a937a6868327f5758b30793a4d
foasho <foa.sho0111@outlook.jp>
2022-02-10 00:02:20 +0900

    update: マイク処理の追加

--- README.md
+++ README.md
@@ -1,7 +1,31 @@
 # 環境構築
+- **Python**:3.7.3
+## 現在の"python"がバージョン３系になっているか確認
 ```
+python
+```
+** ちがかった場合、以下で更新する **
+```
+cd /usr/bin
+sudo rm python
+sudo ln -s python3 python
+cd /usr/local/lib
+python
+```
+
+## 必要なものをインストール
+```
+sudo apt-get update
+pip install mpu6050-raspberrypi
+sudo apt-get install -y libqt5gui5 python3-dev python3-pyqt5
+sudo apt-get install -y libatlas-base-dev libjasper-dev
+```
+
+```
+sudo mkdir python3.7
 pip install -r requirements.txt
 ```
+
 
 ## セットアップ
 ### 合成音声セットアップ
@@ -12,4 +36,17 @@ sudo chmod 755 jtalk.sh
 unzip MMDAgent_Example-1.7.zip
 sudo cp -R ./MMDAgent_Example-1.7/Voice/mei /usr/share/hts-voice/
 ./jtalk.sh "こんにちは。ラズベリーパイが喋っていますよ"
+```
+
+### マイクのセットアップ
+```
+arecord -l
+>> カード番号を確認
+amixer -D hw:1 sset Mic 100%
+```
+
+### 音声認識セットアップ
+```
+pip install pyaudio
+sudo apt install libportaudio0 libportaudio2 libportaudiocpp0 portaudio19-dev
 ```
---
+++ hard_controls/mic_controls.py
@@ -0,0 +1,119 @@
+import pyaudio
+import wave
+import os
+from datetime import date, datetime
+import numpy as np
+import time
+
+"""
+Settings
+"""
+class RecordType:
+    Seconds = 0
+    WhileThreads = 1
+form_1 = pyaudio.paInt16 # 16-bit resolution
+chans = 1 # 1 channel
+samp_rate = 44100 # 44.1kHz　サンプリング周波数
+chunk = 4096 # 2^12 一度に取得するデータ数
+dev_index = 2 # デバイス番号
+wav_output_filename = 'test.wav' # 出力するファイル
+
+audio = pyaudio.PyAudio() # create pyaudio instantiation
+
+def mic_get_audio_stream(
+    record_type=RecordType.Seconds, 
+    record_secs=3,
+    record_thread=0.01,
+    is_save=False, 
+    output_name=wav_output_filename
+):
+    # create pyaudio stream
+    stream = audio.open(format = form_1,rate = samp_rate,channels = chans, \
+                        input_device_index = dev_index,input = True, \
+                        frames_per_buffer=chunk)
+    print("recording")
+    frames = []
+
+    # loop through stream and append audio chunks to frame array
+    if record_type == RecordType.Seconds:
+        for i in range(0,int((samp_rate/chunk)*record_secs)):
+            data = stream.read(chunk, exception_on_overflow=False)
+            frames.append(data)
+        # save the audio frames as .wav file
+        if is_save:
+            output_dir = "./output/"
+            if not os.path.exists(output_dir):
+                os.makedirs(output_dir)
+            wavefile = wave.open(output_dir+output_name,'wb')
+            wavefile.setnchannels(chans)
+            wavefile.setsampwidth(audio.get_sample_size(form_1))
+            wavefile.setframerate(samp_rate)
+            wavefile.writeframes(b''.join(frames))
+            wavefile.close()
+    else:
+        while True:
+            try:
+                threshold = record_thread
+                # 音データの取得
+                data = stream.read(chunk,exception_on_overflow = False)
+                # ndarrayに変換
+                x = np.frombuffer(data, dtype="int16") / 32768.0
+                print(x.max())
+
+                # 閾値以上の場合はファイルに保存
+                if x.max() > threshold:
+                    # 2秒間の音データを取込
+                    frames = []
+                    # all.append(data)
+                    for i in range(0,int((samp_rate/chunk)*record_secs)):
+                        data = stream.read(chunk,exception_on_overflow = False)
+                        frames.append(data)
+
+                    # 音声ファイルとして出力
+                    if is_save:
+                        now = datetime.now()
+                        filename = now.strftime("%Y%m%d%H%M%S") + ".wav"
+                        output_dir = "./output/"
+                        if not os.path.exists(output_dir):
+                            os.makedirs(output_dir)
+                        wavefile = wave.open(output_dir+filename,'wb')
+                        wavefile.setnchannels(chans)
+                        wavefile.setsampwidth(audio.get_sample_size(form_1))
+                        wavefile.setframerate(samp_rate)
+                        wavefile.writeframes(b''.join(frames))
+                        wavefile.close()
+                        print("Saved.")
+                # time.sleep(0.01)
+            except KeyboardInterrupt:
+                print("Ctrl+Cが押されました。")
+                break
+
+    print("finished recording")
+
+    # stop the stream, close it, and terminate the pyaudio instantiation
+    stream.stop_stream()
+    stream.close()
+    audio.terminate()
+
+if __name__ == "__main__":
+    print("start test mic")
+    now = datetime.now()
+    filename = now.strftime('%Y%m%d%H%M%S') + ".wav"
+    print(filename)
+    # 単発録音
+    # mic_get_audio_stream(
+    #     record_type=RecordType.Seconds, 
+    #     record_secs=10, 
+    #     record_thread=0.01, 
+    #     is_save=True, 
+    #     output_name=filename
+    # )
+
+    # 一定の音量を超えたら録音
+    mic_get_audio_stream(
+        record_type=RecordType.WhileThreads, 
+        record_secs=10, 
+        record_thread=0.005, 
+        is_save=True, 
+        output_name=filename
+    )
